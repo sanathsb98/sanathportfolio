ca08f6040cf98531c3be791edd13b26523bf89bb
sanathsb99 <sanathkarthika@gmail.com>
2023-05-01 13:42:01 +0530

    wip: 3d structure implemented

--- src/components/globe/globe.jsx
+++ src/components/globe/globe.jsx
@@ -11,25 +11,51 @@ const Globe = () => {
 
 
 useEffect(() => {
-//image:
+//image1:
 var textureLoader = new THREE.TextureLoader();
-var backgroundTexture = textureLoader.load('./src/assets/background.jpg');
+var backgroundTexture = textureLoader.load('./src/assets/sun.jpg');
 var backgroundGeometry = new THREE.PlaneGeometry(2, 2, 0);
 
+//image2:
+var textureLoader2 = new THREE.TextureLoader();
+var backgroundTexture2 = textureLoader2.load('./src/assets/earth.jpg');
+var backgroundGeometry2 = new THREE.PlaneGeometry(2, 2, 0);
 
-
+//image2:
+var textureLoader3 = new THREE.TextureLoader();
+var backgroundTexture3 = textureLoader3.load('./src/assets/moon.jpg');
+var backgroundGeometry3 = new THREE.PlaneGeometry(2, 2, 0);
 
 //scene:
 const scene = new THREE.Scene();
-scene.background = new THREE.Color("#000000");
+scene.background = new THREE.Color("#000019");
 
-//geometry:
+
+//geometry1:
 const geometry = new THREE.SphereGeometry( 3,30,30 ); 
-const material = new THREE.MeshStandardMaterial( {   map: backgroundTexture} ); 
+const material = new THREE.MeshBasicMaterial( {   map: backgroundTexture} ); 
 var backgroundMesh = new THREE.Mesh(backgroundGeometry, material);
 const mesh = new THREE.Mesh( geometry, material );
 scene.add(backgroundMesh);
 scene.add(mesh);
+
+//geometry2:
+const geometry2 = new THREE.SphereGeometry( 3,30,30 ); 
+const material2 = new THREE.MeshStandardMaterial( {   map: backgroundTexture2} ); 
+var backgroundMesh2 = new THREE.Mesh(backgroundGeometry2, material2);
+const mesh2 = new THREE.Mesh( geometry2, material2 );
+mesh2.position.set( 10, 5, 0 ); // set the position of the first sphere
+scene.add(backgroundMesh2);
+scene.add(mesh2);
+
+//geometry3:
+const geometry3 = new THREE.SphereGeometry( 3,30,30 ); 
+const material3 = new THREE.MeshStandardMaterial( {   map: backgroundTexture3} ); 
+var backgroundMesh3 = new THREE.Mesh(backgroundGeometry3, material3);
+const mesh3 = new THREE.Mesh( geometry3, material3 );
+mesh3.position.set( -30, 15, 10 ); // set the position of the first sphere
+scene.add(backgroundMesh3);
+scene.add(mesh3);
 
 //sizes:
 const sizes = {
@@ -38,7 +64,7 @@ const sizes = {
 }
 
 //light:
-const light = new THREE.PointLight(0xffffff, 1, 100)
+const light = new THREE.PointLight(0xffffff, 1, 200)
 light.position.set(25,25,25)
 scene.add(light)
 
@@ -63,7 +89,7 @@ controls.enableDamping = true;
 controls.enablePan = false;
 controls.enableZoom = false;
 controls.autoRotate = true;
-controls.autoRotateSpeed = 15;
+controls.autoRotateSpeed = 10;
 
 //resize:
 window.addEventListener('resize',()=>{
@@ -93,8 +119,7 @@ t1.fromTo(mesh.scale,{z:0,x:0,y:0},{z:1,x:1,y:1})
     <>
     <h1 className='globeTitle'>Hi Im Sanath SB</h1><br/>
     <p className='desTitle'>I am a frontend developer</p>
-    <canvas className='webgl'>
-    </canvas>
+    <canvas className='webgl'></canvas>
     </>
   )
 }
